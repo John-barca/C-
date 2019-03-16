50f9bfbbb8edcbc9b1020930e93dfbd403bc0e53
John-barca <1113903029@qq.com>
2019-03-16 14:33:10 +0800

    计算日期到天数的转换

---
+++ 计算日期到天数的转换/计算日期到天数的转换/text.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int days[13] = { 0,31,59,90,120,151,181,212,243,273,304,334,365 };
+	int year, month, day;
+	while (cin >> year >> month >> day)
+		//前几个月的天数+这个月（day【month-1】+day）
+	{
+		int total = days[month - 1] + day;
+		if (month > 2)
+		{
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+			{
+				total += 1;
+			}
+		}
+		cout << total << endl;
+	}
+	system("pause");
+	return 0;
+}
