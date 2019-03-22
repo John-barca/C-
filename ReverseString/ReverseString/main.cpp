91afde0b8e56c22b8a33594b07bfc1a60a20ce0d
John-barca <1113903029@qq.com>
2019-03-22 11:15:38 +0800

    字符串逆置

---
+++ ReverseString/ReverseString/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	void reverseString(vector<char>& s) {
+		int begin = 0;
+		int end = s.size() - 1;
+		while (begin < end)
+		{
+			swap(s[begin], s[end]);
+			begin++;
+			end--;
+		}
+	}
+};
